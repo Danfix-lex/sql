ae6f8d2794a1e66c57cfcfd3d8bec61ffda65767
Danfix-lex <danielojolex@gmail.com>
2025-03-19 18:51:20 +0100

    sql

--- sqlTask/class_task_two.sql
+++ sqlTask/class_task_two.sql
@@ -4,3 +4,6 @@ SELECT i.SKU, i.SKU_Description, w.WarehouseID, w.WarehouseCity, w.WarehouseStat
 INVENTORY i JOIN WAREHOUSE w ON i.WarehouseID = w.WarehouseID WHERE w.WarehouseCity != 'Atlanta' AND w.WarehouseCity != 'Bangor' AND w.WarehouseCity != 'Chicago';
 SELECT i.SKU, i.SKU_Description, w.WarehouseID, w.WarehouseCity, w.WarehouseState FROM
 INVENTORY i JOIN WAREHOUSE w ON i.WarehouseID = w.WarehouseID WHERE w.WarehouseCity NOT IN ('Atlanta', 'Bangor', 'Chicago');
+SELECT SKU, SKU_Description, WarehouseID FROM INVENTORY WHERE WarehouseID IN (SELECT WarehouseID FROM WAREHOUSE WHERE Manager = 'Lucille Smith');
+SELECT SKU, SKU_Description, INVENTORY.WarehouseID FROM INVENTORY, WAREHOUSE WHERE INVENTORY.WarehouseID = WAREHOUSE.WarehouseID AND WAREHOUSE.Manager = 'Lucille Smith';
+
